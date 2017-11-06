906d5c29bcd61cf4b9ba626731e6b584702f515c
Lia Hiscock <liahiscock@outlook.com>
2017-11-06 08:55:07 -0800

    moved code from ConsoleApp1 to chatbotdata

--- AddChatbotData/Program.cs
+++ AddChatbotData/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace AddChatbotData
 {
@@ -10,7 +11,44 @@ namespace AddChatbotData
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hey");
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "holynamesacademy.database.windows.net";
+                builder.UserID = "hna-admin";
+                builder.Password = "CharityAndWisdom1";
+                builder.InitialCatalog = "hna-db";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT *");
+                    sb.Append("FROM HNAEvents");
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0}");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+
+
         }
     }
 }
