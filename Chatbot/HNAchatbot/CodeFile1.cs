685836c65fc86f70328dad95062b1cda17d959c3
Haley <hgwelliver@gmail.com>
2018-02-05 07:35:22 -0800

    added messages based on keywords

--- Chatbot/HNAchatbot/CodeFile1.cs
+++ Chatbot/HNAchatbot/CodeFile1.cs
@@ -10,25 +10,30 @@ namespace HNAchatbot {
 
         public static string SendMessage(string message)
         {
-            if (message == "hi")
+            if (message.Contains("hi") || message.Contains("hello"))
             {
                 return "Welcome to the HNA Chatbot!\nSome things you can ask me are:\nWhat's the schedule today?" +
                 "\nWhat's the lunch menu today?\nWhat sports games are today?\nAre there any dances coming up? Type \"END\" to stop at anytime.";
             }
 
-            if (message == "calendar")
+            if (message.Contains("calendar"))
             {
                 return "sah dude";
             }
 
-            else if (message == "lunch")
+            else if (message.Contains("lunch"))
             {
                 return "i love food";
             }
 
-            else if (message == "dance")
+            else if (message.Contains("dance"))
             {
                 return "goteem";
+            }
+
+            else if (message.Contains("game"))
+            {
+                return "sport";
             }
 
             else
