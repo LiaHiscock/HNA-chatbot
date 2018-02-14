6ae7c55a9cf1e7d725f3837cea048007ae7d6b39
Katja <kroberts18@holynames-sea.org>
2018-02-14 07:50:18 -0800

    created method to parse the ICal file

--- AddChatbotData/Program.cs
+++ AddChatbotData/Program.cs
@@ -10,7 +10,7 @@ namespace AddChatbotData
 {
     class Program
     {
-        static void Main(string[] args)
+        static void AddDataToDatabase(string[] args)
         {
             try
             {
@@ -41,25 +41,41 @@ namespace AddChatbotData
                             var line = reader.ReadLine().Trim();
                             var values = line.Split(',');
 
-                            
+
                             String sql = $"INSERT INTO HNAEvents(EventId, Name, DateTime, Location, Type, ExtraNotes) VALUES ('{values[0]}', '{values[1]}', '{values[2]}', '{values[3]}', '{values[4]}', '{values[5]}')";
-                                                      
+
                             SqlCommand insertCommand = new SqlCommand(sql, connection);
                             var rowsAffected = insertCommand.ExecuteNonQuery();
                             Console.WriteLine(rowsAffected);
-                                                 
-                                                                            
+
+
                             //successful query -- String sql = "INSERT INTO HNAEvents(EventId, Name, DateTime, Location, Type, ExtraNotes) 
-                            //                                  VALUES(1, 'Deck the Dome', '2017-10-28 08:00', 'holy names', 'auction', 'none')";                            
-                            
+                            //                                  VALUES(1, 'Deck the Dome', '2017-10-28 08:00', 'holy names', 'auction', 'none')";                           
                         }
-                    }                    
+                    }
                 }
             }
             catch (SqlException e)
             {
                 Console.WriteLine(e.ToString());
             }
+        }
+
+        static void AddDataToCSV(string[] args)
+        {
+            
+            String datafile = @".\data\data.csv";
+            String ical = @".\data\hna - calendar.ics";
+
+            using (var reader = new StreamReader(ical))
+                    {
+                       
+                        while (!reader.EndOfStream)
+                        {
+                            
+                 
+                        } 
+
         }
     }
 }
